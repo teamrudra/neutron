89e1adcbe7540049e24ae2474162cbeedd804ef0
Vaibhav Shelke <vaibhavshelke017@gmail.com>
2018-03-24 01:55:06 +0530

    new feature added

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,5 +1,5 @@
 var sendData = require('./communication').sendData;
-var host = "192.168.1.39";
+var host = "0.0.0.0";
 var keyMap = { "w":false , "a": false, "s":false, "d":false ,
                "q":false , "e":false , "r":false , "Control":false , 
                "ArrowLeft":false , "ArrowRight":false , "ArrowDown":false , "ArrowUp":false ,
@@ -17,7 +17,7 @@ var initKeyboard = function() {
     $("#roverip").keypress(function(event) {
         if (event.key === "Enter")
             host = $("#roverip").val();
-    })
+    });
     setInterval(processKeys, 100);
 }
 
@@ -28,7 +28,7 @@ var processKeys = function() {
     });
     var drive = parseInt(output.substring(0, 8), 2);
     var arm = parseInt(output.substring(8), 2);
-    sendData(host, 3301, "<" + drive + "," + arm + ">");
+    // sendData(host, 3301, "<" + drive + "," + arm + ">");
 }
 
 module.exports.initKeyboard = initKeyboard;
