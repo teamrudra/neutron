f6f9391b07d6a1eba4520268162d9c36d3dad891
Renish <rk080299@gmail.com>
2018-03-24 06:41:34 +0530

    coordinates

--- js/map.js
+++ js/map.js
@@ -1,5 +1,3 @@
-
-
 var tilesDb = {
     getItem: function (key) {
         return localforage.getItem(key);
@@ -51,66 +49,60 @@ var tilesDb = {
     _removeItem: function (key) {
         return localforage.removeItem(key);
     }
-  }
+}
 
 
 
 var offlineLayer = L.tileLayer.offline('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', tilesDb, {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abc',
-  minZoom: 1,
-  maxZoom: 19,
-  crossOrigin: true
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abc',
+    minZoom: 1,
+    maxZoom: 19,
+    crossOrigin: true
 });
-
-
 
 var offlineControl = L.control.offline(offlineLayer, tilesDb, {
-  //saveButtonHtml: '<i class="fa fa-download" aria-hidden="true"></i>',
-  //removeButtonHtml: '<i class="fa fa-trash" aria-hidden="true"></i>',
-  confirmSavingCallback: function (nTilesToSave, continueSaveTiles) {
-      if (window.confirm('Save ' + nTilesToSave + '?')) {
-          continueSaveTiles();
-      }
-  },
-  confirmRemovalCallback: function (continueRemoveTiles) {
-      if (window.confirm('Remove all the tiles?')) {
-          continueRemoveTiles();
-      }
-  },
-  minZoom: 1,
-  maxZoom: 19
+    confirmSavingCallback: function (nTilesToSave, continueSaveTiles) {
+        if (window.confirm('Save ' + nTilesToSave + '?')) {
+            continueSaveTiles();
+        }
+    },
+    confirmRemovalCallback: function (continueRemoveTiles) {
+        if (window.confirm('Remove all the tiles?')) {
+            continueRemoveTiles();
+        }
+    },
+    minZoom: 1,
+    maxZoom: 19
 });
 
+var initMap = function () {
+    var map = L.map('map');
+
+    var m = L.marker([12.821260, 80.038329]).addTo(map);
 
 
-var initMap =function(){
-  var map=L.map('map');
+    var position=L.control.mousePosition();
 
-  var m = L.marker([12.821260, 80.038329]).addTo(map);
+    var coordinates=L.control.coordinates({
+                position:"topright",
+                useDMS:true,
+                labelTemplateLat:"N {y}",
+                labelTemplateLng:"E {x}",
+                useLatLngOrder:true
+            });
 
 
-  offlineControl.addTo(map);
-  offlineLayer.addTo(map);
+    offlineControl.addTo(map);
+    offlineLayer.addTo(map);
+    position.addTo(map);
+    coordinates.addTo(map);
 
-  map.setView({
-    lat: 12.821260,
-    lng: 80.038329
-  }, 18);
+    map.setView({
+        lat: 12.821260,
+        lng: 80.038329
+    }, 50);
 
 }
 
-module.exports=initMap;
-
-
-/*var initMap = function() {
-    var mymap = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoidnNoZWxrZSIsImEiOiJjamYydHA4OGswc2NjMzNsbnJ6N2Jhb2l6In0.Zt_78qmQF6WDcE6MtvPQjA'
-    }).addTo(mymap);
-}
-
-module.exports = initMap;*/
+module.exports = initMap;
