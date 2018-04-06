0a608e8c2f5036ad6c015b95c1b2dc75723fefad
Vaibhav Shelke <vaibhavshelke017@gmail.com>
2018-04-07 01:20:11 +0530

    small chnges

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,9 +1,11 @@
 var sendData = require('./communication').sendData;
 var host = "0.0.0.0";
+var port = 3301;
 var keyMap = { "w":false , "a": false, "s":false, "d":false ,
                "q":false , "e":false , "t":false , "Control":false , 
                "ArrowLeft":false , "ArrowRight":false , "ArrowDown":false , "ArrowUp":false ,
-               "j":false , "k":false , "l": false, "Shift":false };
+               "j":false , "k":false , "l": false, "Shift":false, "i": false, "o": false };
+var allowData = false;
 
 var initKeyboard = function() {
     $('body').keydown(function(event) {
@@ -14,10 +16,17 @@ var initKeyboard = function() {
         if (keyMap.hasOwnProperty(event.key))
             keyMap[event.key] = false;
     });
-    $("#roverip").keypress(function(event) {
-        if (event.key === "Enter")
-            host = $("#roverip").val();
-    });
+    $('#updStatus').click(function(event) {
+        host = $("#roverip").val().split(":")[0];
+        port = $("#roverip").val().split(":")[1];
+        if ($(this).hasClass('btn-warning')) {
+            $(this).removeClass('btn-warning').addClass('btn-positive').html('Stop');
+            allowData = true;
+        } else if ($(this).hasClass('btn-positive')) {
+            $(this).removeClass('btn-positive').addClass('btn-warning').html('Start');
+            allowData = false;
+        }
+    })
     setInterval(processKeys, 1);
 }
 
@@ -27,8 +36,11 @@ var processKeys = function() {
         output += (keyMap[key] ? "1" : "0");
     });
     var drive = parseInt(output.substring(0, 8), 2);
-    var arm = parseInt(output.substring(8), 2);
-    sendData(host, 3301, "<" + drive + "," + arm + ">");
+    var arm = parseInt(output.substring(8, 16), 2);
+    if (allowData) {
+        console.log(drive + " " + arm);
+        sendData(host, port, "<" + drive + "," + arm + ">");
+    }
 }
 
 module.exports = initKeyboard;
