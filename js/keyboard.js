8a0221820cbebec4e72728eb74ef4cbe5f54b24d
LochanRn <rajanaga_ra@srmuniv.edu.in>
2018-04-21 16:04:43 +0530

    Changes in JS file fixing gripper

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,5 +1,5 @@
 var keyMap = { "w":false , "a": false, "s":false, "d":false ,
-               "q":false , "e":false , "t":false , "Control":false , 
+               "q":false , "e":false , "t":false , "Control":false ,
                "ArrowLeft":false , "ArrowRight":false , "ArrowDown":false , "ArrowUp":false ,
                "j":false , "k":false , "l": false, "Shift":false, "i": false, "o": false };
 
@@ -20,9 +20,9 @@ var processKeys = function() {
         output += (keyMap[key] ? "1" : "0");
     });
     if (keyMap['i'])
-        output = output.substring(0, 8) + "11110000";
+        output = output.substring(0, 8) + "00000110";
     if (keyMap['o'])
-        output = output.substring(0, 8) + "00001111";
+        output = output.substring(0, 8) + "00001100";
     var drive = parseInt(output.substring(0, 8), 2);
     var arm = parseInt(output.substring(8, 16), 2);
     return [drive, arm];
@@ -30,4 +30,3 @@ var processKeys = function() {
 
 module.exports.initKeyboard = initKeyboard;
 module.exports.processKeys = processKeys;
-
